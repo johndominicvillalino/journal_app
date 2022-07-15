bb1f2cd8264277c75fd8e0a4b49030361652ae96
JohnDv <johndominicvillalino@outlook.com>
2022-07-15 20:22:47 +0800

    uniqueness validation for category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,7 @@
 class Category < ApplicationRecord
 
     has_many :tasks, :dependent => :destroy
-    validates :name, presence: true, uniqueness: true
+    validates :name, presence: true, uniqueness: { case_sensitive: false }
+    
     
 end
