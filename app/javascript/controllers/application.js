e70803281a763096612a02c840d7dcc825d994b7
JohnDy PC <johndominicvillalino@gmail.com>
2022-07-16 22:59:20 +0800

    implement jquery

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,4 +1,5 @@
 import { Application } from "@hotwired/stimulus"
+import '../custom/home'
 
 const application = Application.start()
 
---
+++ app/javascript/custom/home.js
@@ -0,0 +1,7 @@
+
+import 'jquery'
+
+$('.login-form-container').css("border", "13px solid red")
+
+alert('test')
+
