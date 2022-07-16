0f33b47dc043dbfcc4a956ab8d674563b776974e
JohnDy PC <johndominicvillalino@gmail.com>
2022-07-17 01:30:58 +0800

    vendor compiled assets

---
+++ public/assets/custom/home-afdcc1a1eafbb8043ee4728461b4d0b342c191a05652a04f4670c4732eecc84b.js
@@ -0,0 +1,18 @@
+import "jquery"
+
+$('.back-login').on('click', function() {
+    
+    $('.login-form-container').animate({width:'toggle'},350);
+    
+})
+
+$('.login').on('click', function() {
+    $('.login-form-container').animate({width:'toggle', },350).css('display','flex');
+})
+
+
+
+
+
+
+;
